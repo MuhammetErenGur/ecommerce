ebf656f80bb85b96daa1f0a897f073e6fc431ca1
Muhammet eren Gür <muhammet.erengur@hotmail.com>
2025-02-17 18:22:13 +0300

    Error debug price update 14

--- contentDetails10.js
+++ contentDetails10.js
@@ -43,7 +43,7 @@ function dynamicContentDetails(ob)
     detailsDiv.id = 'details'
 
     let h3DetailsDiv = document.createElement('h3')
-    let h3DetailsText = document.createTextNode('TL 12898.91 ')
+    let h3DetailsText = document.createTextNode('TL 13898.91 ')
     //let h3DetailsText = document.createTextNode('TL 2800 ' + ob.price)
     h3DetailsDiv.appendChild(h3DetailsText)
 
